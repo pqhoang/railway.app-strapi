983d1240f88721b7c5812107add5f1de075c15e9
hoangpq <hoangpq.qmcapital@gmail.com>
2025-02-28 11:39:54 +0700

    test

--- src/api/person/controllers/person.ts
+++ src/api/person/controllers/person.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::person.person');
+// export default factories.createCoreController('api::person.person');
+
+export default factories.createCoreController("api::person.person", ({ strapi }) => ({
+  async create(ctx) {
+    // Nếu request body không có "data", tự động thêm vào
+    if (!ctx.request.body.data) {
+      ctx.request.body = { data: ctx.request.body };
+    }
+
+    // Gọi hàm create mặc định của Strapi
+    return await super.create(ctx);
+  }
+}));
