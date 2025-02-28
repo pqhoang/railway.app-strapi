d6fdd77fcbb2a2317a143ee0b3227126896f2c9d
hoangpq <hoangpq.qmcapital@gmail.com>
2025-02-28 13:37:47 +0700

    test

--- src/api/person/controllers/person.ts
+++ src/api/person/controllers/person.ts
@@ -2,18 +2,36 @@
  * person controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from '@strapi/strapi'
 
 // export default factories.createCoreController('api::person.person');
 
-export default factories.createCoreController("api::person.person", ({ strapi }) => ({
-  async create(ctx) {
-    // Nếu request body không có "data", tự động thêm vào
-    if (!ctx.request.body.data) {
-      ctx.request.body = { data: ctx.request.body };
-    }
+export default factories.createCoreController("api::person.person", ({strapi}) => ({
+    // async create(ctx) {
+    //   // Nếu request body không có "data", tự động thêm vào
+    //   if (!ctx.request.body.data) {
+    //     ctx.request.body = { data: ctx.request.body };
+    //   }
+    //
+    //   // Gọi hàm create mặc định của Strapi
+    //   return await super.create(ctx);
+    // }
+    async create(ctx) {
+        try {
+            // 🛠 Kiểm tra nếu parameters là chuỗi, thì parse thành JSON object
+            if (typeof ctx.request.body.parameters === "string") {
+                ctx.request.body.parameters = JSON.parse(ctx.request.body.parameters);
+            }
 
-    // Gọi hàm create mặc định của Strapi
-    return await super.create(ctx);
-  }
+            const response = await strapi.entityService.create("api::person.person", {
+                data: ctx.request.body.parameters,
+            });
+
+
+            return response;
+        } catch (error) {
+            ctx.throw(400, error);
+        }
+    },
 }));
+
